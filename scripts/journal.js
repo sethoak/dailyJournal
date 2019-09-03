9eaca1522efb48946035ff165ebde1078b01d1d5
sethoak <sethoak@gmail.com>
2019-09-03 13:30:50 -0500

    did journal entry #2

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,6 +1,33 @@
-const journalEntry = {
-  date: "",
-  mode: "",
-  subject: "",
-  entry: ""
-};
+const journalEntry = [
+  {
+    date: "9/2/2019",
+    mood: "One Sad Sally",
+    subject: "I tried to use Flex",
+    entry:
+      "Flex overtook me at the initial lecture. What is fleX? Is it a super hero? I don't know. Outlook drab."
+  },
+  {
+    date: "9/3/2019",
+    mood: "Happy as a clam!",
+    subject: "I defeated FLEX",
+    entry:
+      "The day was cold and I was at my wits end. But I learned. Taught myself. And overcame FLEX."
+  },
+  {
+    date: "9/4/2019",
+    mood: "I feel nothing. Therefore I am nothing",
+    subject: "Javascript",
+    entry: "What is this madness? I'm targeting the DOM. THE DOM!"
+  }
+];
+
+const entryTags = ["date", "mood", "subject", "entry"];
+
+journalEntry.push({
+  date: "9/5/2019",
+  mood: "No pain, no gain. Right?",
+  subject: "THE PAIN",
+  entry: "I gained some knowledge with some pain involved."
+});
+
+console.log(journalEntry, "journalEntry");
